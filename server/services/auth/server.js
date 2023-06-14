a41049579ebe4ea887ea0ecc88c589317efe5968
Sama-E <112719147+Sama-E@users.noreply.github.com>
2023-06-14 11:58:46 -0600

    mongo

--- server/services/auth/server.js
+++ server/services/auth/server.js
@@ -1,7 +1,18 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
+
+mongoose.set('strictQuery', true);
+
+try {
+  await mongoose.connect(process.env.MONGO);
+} catch (error) {
+  handleError(error);
+}
 
 app.listen(8800, () => {
   console.log("Running")
