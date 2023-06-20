4bc86fa3ac1ca543c3fcf06a79441cc575cf417f
Sama-E <112719147+Sama-E@users.noreply.github.com>
2023-06-19 20:30:31 -0600

    register working

--- server/services/auth/controllers/auth.controller.js
+++ server/services/auth/controllers/auth.controller.js
@@ -1,20 +1,22 @@
 import User from "../models/User.js";
+import bcrypt from "bcrypt";
 
 //REGISTER
 export const register = async (req, res) => {
 
   try{
     //New User
+    //Hash Password with Bcrypt
+    const hash = bcrypt.hashSync(req.body.password, 5);
     const newUser = new User({
-      username: "test",
-      email:"test",
-      password: "test",
-      country: "test",
+      ...req.body,
+      password: hash,
     });
 
     await newUser.save();
     res.status(201).send("User has been created.")
   }catch(err){
+
     res.status(500).send("Something went wrong")
   }
 }
@@ -22,6 +24,24 @@ export const register = async (req, res) => {
 
 //LOGIN
 export const login = async (req, res) => {
+  try{
+    //Find user
+    const user = await User.findOne({username: req.body.username});
+    //If user doesn't exist
+    if(!user) return res.status(404).send("User not found!")
+    //Check Password
+    const isCorrect = bcrypt.compareSync(req.body.password, user.password);
+    //If user input password is wrong
+    if(!isCorrect) return res.status(400).send("Wrong username or password!");
+
+    //Successful login - send user credentials 
+    //To homepage w/out password
+    const {password, ...info} = 
+    res.status(200).send(info);
+  } catch(err) {
+
+    res.status(500).send("Something went wrong")
+  }
 
 }
 
--- server/services/auth/server.js
+++ server/services/auth/server.js
@@ -8,6 +8,8 @@ dotenv.config();
 
 const app = express();
 
+app.use(express.json());
+
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 
