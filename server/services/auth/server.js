329b58b575496650d163a45e110773a23dc549fa
Sama-E <112719147+Sama-E@users.noreply.github.com>
2023-06-20 18:34:31 -0600

    client- login/register

--- server/services/auth/server.js
+++ server/services/auth/server.js
@@ -10,6 +10,7 @@ dotenv.config();
 const app = express();
 
 //Middleware
+app.use(cors({origin:"https://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
